0678c312a118b718d5b3046c13aad51139c7176f
Ivan Jijon <ivanjijon@autonomia.digital>
2022-12-19 12:54:14 -0500

    Utils test should not depend on other project packages

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,41 +1,48 @@
 package utils
 
 import (
-	aoc2022 "aoc2022/d1"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type element struct {
+	something int
+}
+
+func NewElementWithSomething(i int) *element {
+	return &element{i}
+}
+
 func Test_RemoveElementFromSlice(t *testing.T) {
 
-	// Each elf must have different calories for this to work
-	e1 := aoc2022.NewElfWithCalories(1)
-	e2 := aoc2022.NewElfWithCalories(2)
-	e3 := aoc2022.NewElfWithCalories(3)
-	e4 := aoc2022.NewElfWithCalories(4)
-	e5 := aoc2022.NewElfWithCalories(5)
+	// Each element must have different calories for this to work
+	e1 := NewElementWithSomething(1)
+	e2 := NewElementWithSomething(2)
+	e3 := NewElementWithSomething(3)
+	e4 := NewElementWithSomething(4)
+	e5 := NewElementWithSomething(5)
 
-	var slice []*aoc2022.Elf
+	var slice []*element
 	slice = append(slice, e1, e2, e3, e4)
 
-	assert.Equal(t, []*aoc2022.Elf{e1, e2, e3, e4}, slice)    // order matters
-	assert.NotEqual(t, []*aoc2022.Elf{e1, e2, e3, e5}, slice) // e5 in stead of e4
-	assert.ElementsMatch(t, []*aoc2022.Elf{e1, e2, e3, e4}, slice)
+	assert.Equal(t, []*element{e1, e2, e3, e4}, slice)    // order matters
+	assert.NotEqual(t, []*element{e1, e2, e3, e5}, slice) // e5 in stead of e4
+	assert.ElementsMatch(t, []*element{e1, e2, e3, e4}, slice)
 
 	// we remove the first element
 	slice = RemoveElementFromSlice(e1, slice)
-	assert.Equal(t, []*aoc2022.Elf{e2, e3, e4}, slice)
+	assert.Equal(t, []*element{e2, e3, e4}, slice)
 
 	// we remove an element in the middle
 	slice = RemoveElementFromSlice(e3, slice)
-	assert.Equal(t, []*aoc2022.Elf{e2, e4}, slice)
+	assert.Equal(t, []*element{e2, e4}, slice)
 
 	// we remove the last element
 	slice = RemoveElementFromSlice(e4, slice)
-	assert.ElementsMatch(t, []*aoc2022.Elf{e2}, slice)
+	assert.ElementsMatch(t, []*element{e2}, slice)
 
 	// we remove an unexisting element
 	slice = RemoveElementFromSlice(e5, slice)
-	assert.ElementsMatch(t, []*aoc2022.Elf{e2}, slice)
+	assert.ElementsMatch(t, []*element{e2}, slice)
 }
