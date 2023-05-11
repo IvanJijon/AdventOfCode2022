7e06843ed8c288ecd6bc256978828d547948b48f
Ivan Jijon <ivanjijon@autonomia.digital>
2023-05-11 01:10:42 -0500

    Changing generic function return type to be coherent with type declaration

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func RemoveElementFromSlice[T comparable, L []*T](e *T, slice L) []*T {
+func RemoveElementFromSlice[T comparable, L []*T](e *T, slice L) L {
 	for k, v := range slice {
 		if v == e {
 			slice = append(slice[:k], slice[k+1:]...)
