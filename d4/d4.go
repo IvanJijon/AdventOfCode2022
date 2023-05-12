f34045083da7826bf30b2d7009046fa5648d4552
Ivan Jijon <ivanjijon@autonomia.digital>
2023-05-12 00:44:37 -0500

    d4 Part One solution

---
+++ d4/d4.go
@@ -0,0 +1,81 @@
+package d4
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// How to solve Part One:
+// For each line (pair of Elves):
+// 	- Retrieve the section assigned to each Elf
+// 	- Check if one sectionis contained by the other
+//  - If that's the case, increment the sum
+//  - Return the sum
+
+type section struct {
+	lowerBound int
+	upperBound int
+}
+
+func newSection(s string) section {
+	bounds := strings.Split(s, "-")
+	lb, _ := strconv.Atoi(bounds[0])
+	ub, _ := strconv.Atoi(bounds[1])
+	return section{
+		lowerBound: lb,
+		upperBound: ub,
+	}
+}
+
+func (s1 section) isContainedBy(s2 section) bool {
+	return s1.lowerBound >= s2.lowerBound && s1.upperBound <= s2.upperBound
+}
+
+func (s1 section) containsOrIsContainedBy(s2 section) bool {
+	return s1.isContainedBy(s2) || s2.isContainedBy(s1)
+}
+
+type sectionAssignmentPair struct {
+	sections [2]section
+}
+
+func newSectionAssignmentPair(s string) (*sectionAssignmentPair, error) {
+	if s == "" {
+		return nil, errors.New("sections cannot be empty")
+	}
+	ss := strings.Split(s, ",")
+
+	return &sectionAssignmentPair{[2]section{newSection(ss[0]), newSection(ss[1])}}, nil
+}
+
+func (sap *sectionAssignmentPair) assignSectionsTo(e1, e2 *elf) {
+	e1.s = sap.sections[0]
+	e2.s = sap.sections[1]
+}
+
+type elf struct {
+	s section
+}
+
+func solvePartOne(inputFile string) (int, error) {
+	f, err := os.Open(inputFile)
+	if err != nil {
+		return -1, err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	var sum int
+	for scanner.Scan() {
+		sap, _ := newSectionAssignmentPair(scanner.Text())
+		e1, e2 := &elf{}, &elf{}
+		sap.assignSectionsTo(e1, e2)
+		if e1.s.containsOrIsContainedBy(e2.s) {
+			sum += 1
+		}
+	}
+	return sum, nil
+}
---
+++ d4/d4_test.go
@@ -0,0 +1,94 @@
+package d4
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_newSectionAssignmentPair_returnsAnErrorWhenTheProvidedPairOfSectionsIsEmpty(t *testing.T) {
+	_, err := newSectionAssignmentPair("")
+	assert.Error(t, err, "sections cannot be empty")
+}
+
+func Test_newSectionAssignmentPair_createsASectionAssignmentWithTheProvidedPairOfSections(t *testing.T) {
+	sap, err := newSectionAssignmentPair("2-4,6-8")
+	assert.Nil(t, err)
+	assert.Equal(t, [2]section{newSection("2-4"), newSection("6-8")}, sap.sections)
+
+	sap, err = newSectionAssignmentPair("5-7,7-9")
+	assert.Nil(t, err)
+	assert.Equal(t, [2]section{newSection("5-7"), newSection("7-9")}, sap.sections)
+}
+
+func Test_newSection_createsANewSectionFromTheProvidedString(t *testing.T) {
+	expected := section{1, 2}
+	actual := newSection("1-2")
+
+	assert.Equal(t, expected, actual)
+	assert.Equal(t, 1, actual.lowerBound)
+	assert.Equal(t, 2, actual.upperBound)
+}
+
+func Test_assignSectionsTo_assignsTheCorrespondingSectionToEachElf(t *testing.T) {
+	sap, _ := newSectionAssignmentPair("2-3,4-5")
+	e1 := &elf{}
+	e2 := &elf{}
+
+	sap.assignSectionsTo(e1, e2)
+
+	assert.Equal(t, newSection("2-3"), e1.s)
+	assert.Equal(t, newSection("4-5"), e2.s)
+}
+
+func Test_isContainedIn_returnsTrueIfASectionIsContainedByAnotherFalseIfNot(t *testing.T) {
+	s1 := newSection("1-1") //1.........
+	s2 := newSection("5-5") //....5.....
+	assert.False(t, s1.isContainedBy(s2))
+
+	s1 = newSection("5-6") //....56....
+	s2 = newSection("7-8") //......78..
+	assert.False(t, s1.isContainedBy(s2))
+
+	s1 = newSection("4-8") //...45678..
+	s2 = newSection("5-7") //....567...
+	assert.False(t, s1.isContainedBy(s2))
+
+	s1 = newSection("5-7") //....567...
+	s2 = newSection("5-7") //....567...
+	assert.True(t, s1.isContainedBy(s2))
+
+	s1 = newSection("5-7") //....567...
+	s2 = newSection("4-8") //...45678..
+	assert.True(t, s1.isContainedBy(s2))
+}
+
+func Test_containsOrIsContainedBy_returnsTrueIfASectionIsContainedByAnotherFalseIfNot(t *testing.T) {
+	s1 := newSection("1-1") //1.........
+	s2 := newSection("5-5") //....5.....
+	assert.False(t, s1.containsOrIsContainedBy(s2))
+
+	s1 = newSection("5-6") //....56....
+	s2 = newSection("7-8") //......78..
+	assert.False(t, s1.containsOrIsContainedBy(s2))
+
+	s1 = newSection("4-8") //...45678..
+	s2 = newSection("5-7") //....567...
+	assert.True(t, s1.containsOrIsContainedBy(s2))
+
+	s1 = newSection("5-7") //....567...
+	s2 = newSection("5-7") //....567...
+	assert.True(t, s1.containsOrIsContainedBy(s2))
+
+	s1 = newSection("5-7") //....567...
+	s2 = newSection("4-8") //...45678..
+	assert.True(t, s1.containsOrIsContainedBy(s2))
+}
+
+func Test_solvePartOne(t *testing.T) {
+	r, _ := solvePartOne("test_input1")
+	assert.Equal(t, 2, r)
+
+	r, _ = solvePartOne("input")
+	assert.Equal(t, 477, r)
+}
